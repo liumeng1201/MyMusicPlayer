436eff233181a3672619c36721b2baae3e870695
liumeng <liumeng1201@gmail.com>
2013-03-04 23:25:41 +0800

    modify PlayActivity and MusicService add PlayActivity's layout xml file

--- musicplayerDemo/src/com/lm/musicplayerdemo/PlayActivity.java
+++ musicplayerDemo/src/com/lm/musicplayerdemo/PlayActivity.java
@@ -1,7 +1,25 @@
 package com.lm.musicplayerdemo;
 
 import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
 
 public class PlayActivity extends Activity {
+	private String mTitle;
+	private String mArtist;
+	private int mCurrentTime;
+	private int mDuration;
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		// TODO Auto-generated method stub
+		super.onCreate(savedInstanceState);
+
+		Intent intent = getIntent();
+		mTitle = intent.getStringExtra(Util.MUSIC_TITLE);
+		mArtist = intent.getStringExtra(Util.MUSIC_ARTIST);
+		mCurrentTime = intent.getIntExtra(Util.MUSIC_CURRENTTIME, 0);
+		mDuration = intent.getIntExtra(Util.MUSIC_DURATION, 0);
+	}
 
 }
