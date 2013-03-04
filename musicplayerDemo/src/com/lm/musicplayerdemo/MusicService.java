e9b816b1a8d705af0aa3e9651c196806162047ab
liumeng <liumeng@realaction>
2013-03-04 20:19:01 +0800

    modify

--- musicplayerDemo/src/com/lm/musicplayerdemo/MusicService.java
+++ musicplayerDemo/src/com/lm/musicplayerdemo/MusicService.java
@@ -1,6 +1,11 @@
 package com.lm.musicplayerdemo;
 
+import android.app.Notification;
+import android.app.Notification.Builder;
+import android.app.NotificationManager;
+import android.app.PendingIntent;
 import android.app.Service;
+import android.content.Context;
 import android.content.Intent;
 import android.media.MediaPlayer;
 import android.os.IBinder;
@@ -9,6 +14,7 @@ public class MusicService extends Service implements
 		MediaPlayer.OnCompletionListener {
 	// MediaPlayer对象
 	private MediaPlayer mMediaPlayer;
+
 	// 音频文件总时间
 	private int mDuration;
 	// 当前播放时间
@@ -23,6 +29,9 @@ public class MusicService extends Service implements
 	private String mAlbum;
 	// 歌曲文件路径
 	private String mPath;
+
+	private static Notification mNotification;
+	private static NotificationManager mNotificationManager;
 
 	/**
 	 * 对MusicService进行初始化
@@ -84,6 +93,44 @@ public class MusicService extends Service implements
 			mMediaPlayer.release();
 			mMediaPlayer = null;
 		}
+	}
+
+	/**
+	 * @return 当前播放到的时间
+	 */
+	private int getCurrentTime() {
+		int current = 0;
+		if (mMediaPlayer != null) {
+			current = mMediaPlayer.getCurrentPosition();
+		}
+		return current;
+	}
+
+	/**
+	 * 显示通知栏
+	 */
+	private void showNotification() {
+		// 获取系统通知栏服务对象
+		mNotificationManager = (NotificationManager) (MusicService.this)
+				.getSystemService(Context.NOTIFICATION_SERVICE);
+
+		Builder builder = new Notification.Builder(MusicService.this);
+		// 设置通知栏要显示的内容
+		builder.setSmallIcon(R.drawable.beats_logo_s);
+		builder.setContentTitle(mTitle);
+		builder.setContentText(mArtist);
+		builder.setAutoCancel(false);
+		builder.setWhen(System.currentTimeMillis());
+
+		mCurrentTime = getCurrentTime();
+		Intent intent = new Intent(MusicService.this, PlayActivity.class);
+//		设置要传递的数据
+		PendingIntent contentIntent = PendingIntent
+				.getActivity(MusicService.this, 0, intent,
+						PendingIntent.FLAG_UPDATE_CURRENT);
+		builder.setContentIntent(contentIntent);
+		mNotification = builder.getNotification();
+		mNotificationManager.notify(0, mNotification);
 	}
 
 	@Override
